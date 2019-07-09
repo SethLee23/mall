98a77479127819b0c4b54d22ad240bf196d3ffe6
Seth <1532084282@qq.com>
2019-07-09 17:08:38 +0800

    解决全选按钮的bug

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -14,36 +14,39 @@ new Vue({
     checked: false,
     editing: false,
     total: 0,
+    removePopout: false,
     // editingMsg: '编辑',
   },
   mounted() {
     this.getCartList()
   },
   computed: {
-    removeLists(){
-      if(this.editing){
+    removeLists() {
+      if (this.editing) {
         let arr = []
-        this.editingShop.goodsList.forEach(good=>{
-           if(good.removeChecked){
-               arr.push(good)
-           }
-           } )
-           return arr
+        this.editingShop.goodsList.forEach(good => {
+          if (good.removeChecked) {
+            arr.push(good)
+          }
+        })
+        return arr
       }
       return []
-      
+
     },
-    removeAllChecked:{
-      get(){
-       return this.cartList.some(shop=>{
-          return shop.removeChecked
+    removeAllChecked: {
+      get() {
+        return this.cartList.some(shop => {
+          if(shop.editing){
+            return shop.removeChecked
+          }
         })
       },
-      set(newVal){
-        this.cartList.forEach(shop=>{
-          if(shop.editing){
+      set(newVal) {
+        this.cartList.forEach(shop => {
+          if (shop.editing) {
             shop.removeChecked = newVal
-            shop.goodsList.forEach(good=>{
+            shop.goodsList.forEach(good => {
               good.removeChecked = newVal
             })
           }
@@ -85,9 +88,55 @@ new Vue({
     }
   },
   methods: {
+    hidePopout(){
+      this.removePopout = false
+    },
+    confirmRemove() {
+      if (this.removeData) {
+        let {
+          shop,
+          shopIndex,
+          good,
+          goodIndex
+        } = this.removeData
+        shop.goodsList.splice(goodIndex, 1)
+        if (shop.goodsList.length < 1) {
+          shop.editing = false
+          this.cartList.forEach((s, i) => {
+            if (i !== shopIndex) {
+              s.editingMsg = '编辑'
+            }
+          })
+          this.cartList.splice(shopIndex, 1)
+        }
+        this.removePopout = false
+      } else {
+        let mList = this.removeLists
+        console.log('mList')
+        console.log(mList)
+        console.log(this.editingShop)
+        console.log(this.editingShop)
+
+      }
+
+    },
+    mRemove() {
+      console.log(this.removeLists)
+      this.removePopout = true
+    },
+    deleteGoods(shop, shopIndex, good, goodIndex) {
+      this.removePopout = true
+      this.removeData = {
+        shop,
+        shopIndex,
+        good,
+        goodIndex
+      }
+      console.log(this.removeData)
+    },
     removeShop(shop, shopIndex) {
       shop.removeChecked = !shop.removeChecked
-      shop.goodsList.forEach(good=>{
+      shop.goodsList.forEach(good => {
         good.removeChecked = shop.removeChecked
       })
     },
@@ -102,7 +151,7 @@ new Vue({
     },
     removeCheckAll() {
       console.log("removeAll")
-     this.removeAllChecked = !this.removeAllChecked
+      this.removeAllChecked = !this.removeAllChecked
     },
     reduce(good) {
       if (good.number <= 1) return
